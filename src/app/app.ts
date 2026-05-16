9f56d5a5072e3da6f1ce1aaf3d9b12db48badd4e
geckoFooBar <mudholkarrohan23@gmail.com>
2026-05-16 10:36:21 +0530

    feat: complete modern UI and QR logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,14 @@
 import { Component } from '@angular/core';
-
 import { GenerateQr } from './features/components/generate-qr/generate-qr';
-import { Description } from './features/components/description/description';
-import { Header } from './features/components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GenerateQr, Description, Header],
+  imports: [GenerateQr],
   template: `
-  <div class="flex flex-col min-h-screen">
-    <app-header></app-header>
-    <div class="flex-grow">
-      <app-description></app-description>
-      <app-generate-qr></app-generate-qr>
-    </div>
-  </div>
+    <app-generate-qr></app-generate-qr>
   `,
 })
 export class App {
-  title: string = 'video2QR';
+  title = 'video2QR';
 }
